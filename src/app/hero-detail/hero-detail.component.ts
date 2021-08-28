708634b09eaa65d8ecfffa59c018d71f71404766
VascoMonteiroNeto <vascomonteironeto@gmail.com>
2021-08-28 12:34:26 -0300

    Services, routing e components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -14,9 +19,20 @@ export class HeroDetailComponent implements OnInit {
   //   this.selectedHero = hero;
   // }
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
